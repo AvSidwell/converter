36c5e41c50b6b4b9afb6d5803c7cd37bbe52d5d0
AvSidwell <sidwellavuyilebatyi@gmail.com>
2023-05-25 10:52:31 +0200

    commit

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 let result = document.getElementById('result');
 //let input = document.getElementById('degrees');
-let degrees = document.getElementById('degrees')
+let degreesInput = document.getElementById('degrees')
 //degrees = parseFloat(degrees);
 
 function calculation() {
     //eval works better (changing strings to numbers)
- degrees = eval(degrees.value);
- 
-    // result = (degrees -32 Math.pow(height / 100, 2)).toFixed(1);
-    // document.getElementById("result").innerHTML = (degrees - 32) / 1.8;
-
-    document.getElementById("result").innerHTML = (degrees - 32) / 1.8 +  '°C';
+    degrees = eval(degreesInput.value);
+    document.getElementById("result").innerHTML = (degrees - 32) / 1.8 + '°C';
+    console.log(degrees.value);
 }
+let del = () => {
+    document.getElementById("result").innerHTML = '';
+    degreesInput.value = ''; 
+}
+//function ddel() {
+  //  document.getElementById("degrees").innerHTML = '';
+//}
+//let ddel = () => {
+  //  degrees.value = '';
+//};
